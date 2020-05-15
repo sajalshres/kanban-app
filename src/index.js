5ba1d054739b15d0c13937125175fd038051fc21
rsnjoshi <roshan.joshi@cotiviti.com>
2020-05-15 13:07:42 +0000

    model fetch and delete

--- src/index.js
+++ src/index.js
@@ -1,16 +1,98 @@
 var Backbone = require('backbone');
 var Model = require('./models/model');
 var Collection = require('./collections/collection')
-var View = require('./views/view')
+var _ = require('underscore')
+var $ = require('jquery')
 
-var tasksTodo = new Collection.Tasks([
-    new Model.Task({title: 'github'}),
-    new Model.Task({title: 'docker'})
-])
 
-var todosViews = new View.Todos({el:"#tasks", model: tasksTodo})
 
-todosViews.render();
+
+
+var TodosView = Backbone.View.extend({
+    initialize: function () {
+        this.model.on("remove", this.render, this);
+    },
+    updater:function(){
+        
+    },
+    render: function () {
+        console.log('chalyo')
+        this.$el.html("");
+        var template = _.template($("#inputTemplate").html());
+        var html = template(this.model.toJSON());
+        this.$el.html(html);
+        var self = this;
+        this.model.each(function (task) {
+            var todoView = new TodoView({ model: task });
+            self.$el.append(todoView.render().$el)
+        })
+    }
+})
+
+var TodoView = Backbone.View.extend({
+    events: {
+        "click": "onClick",
+        "click .remove": "onClickRemove"
+    },
+    onClick: function () {
+        console.log("hello clicked")
+    },
+    onClickRemove: function (e) {
+        e.stopPropagation();
+        console.log('btn clicked');
+        tasksTodo.remove(this.model); 
+        this.model.destroy();
+        // this.model.set("title", "haha")
+        // this.remove();
+        // this.model.destroy();
+        // this.model.save();
+        // fetcher();
+        console.log(tasksTodo)
+    },
+    tagName: "div",
+    className: "sdfads",
+    attributes: function () {
+        return {
+            id: this.model.get('title')
+        }
+    },
+    initialize: function () {
+        this.model.on("change", this.render, this);
+        console.log('hello')
+        console.log(this.model.toJSON().title)
+        this.id = this.model.toJSON().title;
+    },
+    render: function () {
+        var template = _.template($("#todoTemplate").html());
+        var html = template(this.model.toJSON());
+        this.$el.html(html);
+        return this;
+    }
+})
+
+var tasksTodo = new Collection.Tasks()
+
+var fetcher = function(){
+    tasksTodo.fetch({
+        success:function(){
+            (new TodosView({el: "#todo", model: tasksTodo})).render();
+        },
+        error:function(){
+            console.log("server down");
+        }
+    })
+}
+
+fetcher();
+
+
+
+
+
+
+// var todo = new Model.Task({title: 'github setup'});
+// var todoView = new View.Todo({el:"#todo", model:todo})
+// todoView.render();
 
 
 
--- src/models/model.js
+++ src/models/model.js
@@ -2,7 +2,9 @@ var Backbone = require('backbone')
 
 var models = {
     "Task" : Backbone.Model.extend({
-        url: 'http://localhost:3000/tasks',
+        url: function(){
+            return 'http://localhost:3000/tasks/' + this.id;
+        },
         initialize: function(){
             console.log("Task created");
         }
