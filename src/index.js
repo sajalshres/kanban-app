fb4809cb17ce56fb86d016dd960c188427e871f1
rsnjoshi <roshan.joshi@cotiviti.com>
2020-05-17 17:10:21 +0000

    Create, Read and Delete

--- src/index.js
+++ src/index.js
@@ -8,54 +8,50 @@ var TodosView = Backbone.View.extend({
     events: {
         'change input#newTask': 'loadTask'
     },
-
-
-    loadTask: function(event) {
+    loadTask: function (event) {
         var val = $(event.currentTarget).val();
         var newModel = new Model.Task();
-        
+        var self = this;
         newModel.set("title", val);
-        newModel.set("status", "todo");
-        
-        newModel.save(null,{
-            success:function(){
+        newModel.set("status", self.myPublic);
+        newModel.save(null, {
+            success: function () {
                 console.log("model saved")
-                tasksTodo.fetch();
+                tasks.fetch();
             },
-            error:function(){
+            error: function () {
                 console.log("model saving failed")
             }
         })
-      
-    }
-,
-    initialize: function () {
+    },
+    initialize: function (option) {
+        this.myPublic = option.option;
         this.model.on("remove", this.render, this);
         this.model.on("add", this.render, this);
     },
-    updater:function(){
-        
-    },
     render: function () {
+        console.log(this.myPublic)
         this.$el.html("");
         var template = _.template($("#inputTemplate").html());
-        var html = template(this.model.toJSON());
+        var html = template({status: this.myPublic});
         this.$el.html(html);
         var self = this;
         this.model.each(function (task) {
-            var todoView = new TodoView({ model: task });
-            self.$el.append(todoView.render().$el)
+            if (task.get("status") === self.myPublic) {
+                var todoView = new TodoView({ model: task });
+                self.$el.append(todoView.render().$el)
+            }
         })
+        self.$el.append("<br>")
     }
 })
-
 var TodoView = Backbone.View.extend({
     events: {
         "click": "onClick",
         "click .remove": "onClickRemove",
-        
+
     },
-    insert:function(){
+    insert: function () {
         console.log('submit done')
     }
     ,
@@ -65,17 +61,11 @@ var TodoView = Backbone.View.extend({
     onClickRemove: function (e) {
         e.stopPropagation();
         console.log('btn clicked');
-        tasksTodo.remove(this.model); 
+        tasks.remove(this.model);
         this.model.destroy();
-        // this.model.set("title", "haha")
-        // this.remove();
-        // this.model.destroy();
-        // this.model.save();
-        // fetcher();
-        console.log(tasksTodo)
+        console.log(tasks)
     },
     tagName: "div",
-    className: "sdfads",
     attributes: function () {
         return {
             id: this.model.get('title')
@@ -94,41 +84,28 @@ var TodoView = Backbone.View.extend({
         return this;
     }
 })
-
-var tasksTodo = new Collection.Tasks()
-
-var fetcher = function(){
-    tasksTodo.fetch({
-        success:function(){
-            (new TodosView({el: "#todo", model: tasksTodo})).render();
+function checker() {
+    tasks.fetch({
+        success: function () {
+            var tempArray = [];
+            for (var i = 0; i < tasks.length; i++) {
+                if (!(_.contains(tempArray, tasks.at(i).get("status")))) {
+                    tempArray.push(tasks.at(i).get("status"))
+                }
+            }
+            console.log(tempArray)
+            for (i of tempArray) {
+                var selector = "#" + i;
+                var div = document.createElement("div");
+                div.id = i;
+                document.body.appendChild(div);
+                (new TodosView({ el: selector, model: tasks, option: i })).render();
+            }
         },
-        error:function(){
-            console.log("server down");
+        error: function () {
+
         }
     })
 }
-
-fetcher();
-
-
-
-
-
-
-
-
-
-
-// var todo = new Model.Task({title: 'github setup'});
-// var todoView = new View.Todo({el:"#todo", model:todo})
-// todoView.render();
-
-
-
-
-
-
-
-
-
-
+var tasks = new Collection.Tasks();
+checker();
