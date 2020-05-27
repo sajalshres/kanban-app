ec175692c98678ecb6def9dd279b2b38da007b17
rsnjoshi <roshan.joshi@cotiviti.com>
2020-05-27 11:38:14 +0000

    mocked-backend structure updated

--- app/index.js
+++ app/index.js
@@ -3,12 +3,14 @@ var Marionette = require("backbone.marionette");
 var TimeStamp = require("./services/timeNow");
 var TaskCollection = require("./collections/tasks");
 var ColumnCollection = require("./collections/columns");
+var Task = require("./models/task");
 var $ = require("jquery");
 
 console.log(TimeStamp());
 
 var columnCollection = new ColumnCollection();
 var taskCollection = new TaskCollection();
+var bufferTaskCollection = new TaskCollection();
 columnCollection.fetch({
   success: () => {
     taskCollection.fetch({
@@ -29,6 +31,12 @@ columnCollection.fetch({
 
 var element_container = Marionette.LayoutView.extend({
   tagName: "div",
+  events: {
+    "click ": "onClickAdd",
+  },
+  onClickAdd: function () {
+    console.log("clicked");
+  },
   attributes: function () {
     return {
       id: "#" + this.model.get("name"),
@@ -39,16 +47,58 @@ var element_container = Marionette.LayoutView.extend({
 
 var column_container = Marionette.CompositeView.extend({
   tagName: "div",
+
   attributes: function () {
     return {
       id: this.model.get("name"),
     };
+  },
+  events: {
+    "click #addTask": "onClickAdd",
+    "input #newTask": "syncer",
+  },
+
+  onClickAdd: function (event) {
+    event.stopPropagation();
+    var self = this;
+    new Task({
+      name: this.inputVal,
+      visible: true,
+      created_at: TimeStamp(),
+      modified_at: "",
+    }).save(null, {
+      success: () => {
+        bufferTaskCollection.fetch({
+          success: () => {
+            let obj = bufferTaskCollection.pop();
+            self.collection.unshift(obj);
+            let buff_array = self.model.get("items");
+            buff_array.unshift(obj.id);
+            self.model.set("items", buff_array);
+            self.model.set("modified_at", TimeStamp())
+            self.model.save(null, {
+              success: () => {},
+              error: () => {},
+            });
+          },
+          error: () => {},
+        });
+      },
+      error: () => {},
+    });
+  },
+
+  syncer: function (event) {
+    this.inputVal = this.$("#newTask").val();
+    console.log(this.inputVal);
   },
 
   template: require("./templates/column.html"),
   childView: element_container,
   childViewContainer: "div#element",
   initialize: function () {
+    console.log(this);
+    this.inputVal = "";
     var items = this.model.get("items");
     var item_object = new Array();
     for (id of items) {
@@ -63,4 +113,7 @@ var Main_container = Marionette.CompositeView.extend({
   template: require("./templates/container.html"),
   childView: column_container,
   childViewContainer: "div#columns",
+  intitialize: function () {
+    // this.collection.on("change", this.render, this)
+  },
 });
