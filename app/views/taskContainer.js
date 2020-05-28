28c4f44d9b07b3b9a1de74805a3401b835e02619
rsnjoshi <roshan.joshi@cotiviti.com>
2020-05-28 13:27:47 +0000

    task edit and remove

--- app/views/taskContainer.js
+++ app/views/taskContainer.js
@@ -2,6 +2,7 @@ var Marionette = require("backbone.marionette");
 var variables = require("../services/variables");
 var $ = require("jquery");
 var _ = require("underscore");
+var TimeStamp = require("../services/timeNow")
 var taskContainer = Marionette.LayoutView.extend({
   tagName: "div",
   attributes: function () {
@@ -18,7 +19,35 @@ var taskContainer = Marionette.LayoutView.extend({
 
   events: {
     "click #opt": "tempLoader",
-    "click #move": "mover"
+    "click #move": "mover",
+    "input #taskChanger": "syncer",
+    "click #editConform": "editSubmit",
+    "click #editCancel": "canceler"
+  },
+
+  canceler:function(){
+    this.render()
+  },
+
+  editSubmit:function(){
+    var self = this;
+    this.model.set("name", this.myEditValue);
+    this.model.set("modified_at", TimeStamp());
+    this.model.save(null, {
+      success:function(){self.render()},
+      error:function(){}
+    })
+
+  },
+
+  syncer: function(e){
+    this.myEditValue = this.$('#taskChanger').val();
+    console.log(this.myEditValue);
+    if((this.myEditValue === this.model.get("name")) || this.myEditValue === "" ){
+      this.$('#editConform').prop("disabled", true);
+    }else{
+      this.$('#editConform').prop("disabled", false);
+    }
   },
 
   tempLoader:function(e){
@@ -34,9 +63,33 @@ var taskContainer = Marionette.LayoutView.extend({
 
   initialize: function (options) {
     this.myBuffer = options;
+    this.myEditValue = "";
     console.log(options.parent.get("name"));
+    var self = this;
     this.$el.on('input', '#opt', function (){
-        //  if(this.val === "")
+         if(this.value === "edit"){
+           console.log(this.value)
+           self.$("#task").html(`<input id="taskChanger" value="${self.model.get("name")}">
+           <button id="editConform">edit</button>
+           <button id="editCancel">cancel</button>`)
+           self.$('#editConform').prop("disabled", true);
+         }else if(this.value === "remove"){
+          console.log(this.value)
+           var tempArray = options.parent.get("items");
+           tempArray.splice(tempArray.indexOf(self.model.get("id")),1);
+              options.parent.set("items", tempArray);
+              options.parent.set("modified_at", TimeStamp());
+              options.parent.save(null, {
+                success:function(){
+                  self.model.destroy();
+                },
+                error:function(){
+
+                }
+              })
+         }else{
+
+         }
     })
   },
 
